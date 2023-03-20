48bc7a6866c7a988b5621a28f5913e8cceff8518
vinh trinh <c4piddd@gmail.com>
2023-03-20 11:03:14 +0700

    updated edit profile

--- src/components/molecules/common/field-group/index.tsx
+++ src/components/molecules/common/field-group/index.tsx
@@ -3,47 +3,55 @@ import { Field } from "formik";
 import styled from "styled-components";
 
 type Props = {
-    name: string;
-    label?: string;
-    sublabel?: string;
-    invalid?: boolean | string;
-    errorMessage?: string;
-    mb?: string;
-    component: any;
-    children?: JSX.Element;
-}
+  name: string;
+  label?: string;
+  sublabel?: string;
+  invalid?: boolean | string;
+  errorMessage?: string;
+  mb?: string;
+  component: any;
+  children?: JSX.Element;
+};
 const GZFieldGroup = ({
-    name,
-    label = "",
-    sublabel = "",
-    invalid = false,
-    errorMessage = "",
-    mb = "16px",
-    component,
-    children
+  name,
+  label = "",
+  sublabel = "",
+  invalid = false,
+  errorMessage = "",
+  mb = "16px",
+  component,
+  children,
 }: Props) => {
-    return <StyledFormGroup mb={mb} className={invalid ? 'invalid' : ''}>
-        {label && (<StyledLabel className="gz-field-label">{label} {sublabel && (<span>{sublabel}</span>)}</StyledLabel>)}
-        <Field name={name} id={`gz-filed-${name}`} component={component} />
-        {children ? children : <></>}
-        {invalid && errorMessage && (<StyledError className="gz-field-error">{errorMessage}</StyledError>)}
+  return (
+    <StyledFormGroup mb={mb} className={invalid ? "invalid" : ""}>
+      {label && (
+        <StyledLabel className="gz-field-label">
+          {label} {sublabel && <span>{sublabel}</span>}
+        </StyledLabel>
+      )}
+      <Field name={name} id={`gz-filed-${name}`} component={component} />
+      {children ? children : <></>}
+      {invalid && errorMessage && (
+        <StyledError className="gz-field-error">{errorMessage}</StyledError>
+      )}
     </StyledFormGroup>
-}
-export default GZFieldGroup
+  );
+};
+export default GZFieldGroup;
 
-const StyledFormGroup = styled(Box) <{ errorcls: string, mb: string }>`
-  margin-bottom:  ${(props) => props.mb};
-  &.invalid{
+const StyledFormGroup = styled(Box)<{ errorcls: string; mb: string }>`
+  margin-bottom: ${(props) => props.mb};
+  &.invalid {
     .gz-field-label,
     .gz-field-error,
-    .gz-field-file{
-      color:#F04438;
+    .gz-field-file {
+      color: #f04438;
     }
-    input, 
-    select, 
+    input,
+    select,
     textarea,
-    .gz-field-file{
-      border-color:#F04438 !important;
+    .gz-field-file {
+      border-color: #f04438 !important;
     }
   }
 `;
@@ -53,9 +61,9 @@ const StyledLabel = styled(Text)`
   font-size: 16px;
   line-height: 150%;
   letter-spacing: 0.02em;
-  color: #BFCFE8;
-  margin-bottom:6px;
-  span{
+  color: #bfcfe8;
+  margin-bottom: 6px;
+  span {
     color: rgba(255, 255, 255, 0.6);
   }
 `;
@@ -64,9 +72,9 @@ const StyledError = styled(Text)`
   font-size: 13px;
   font-weight: 400;
   line-height: 18px;
-  color: #F04438;
+  color: #f04438;
   letter-spacing: -0.08px;
   margin-left: 16px;
   margin-top: 2px;
   margin-bottom: 0;
-`
+`;
